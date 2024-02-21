304655ec9ee0d87d29fad79a31e6048c443d7228
aravind-segu <aravind.segu@databricks.com>
2024-02-20 18:28:57 -0800

    change workflow

--- my_mlops_project/tests/training/test_notebooks.py
+++ my_mlops_project/tests/training/test_notebooks.py
@@ -4,6 +4,6 @@ import pathlib
 def test_notebook_format():
     # Verify that all Databricks notebooks have the required header
     paths = list(pathlib.Path("./notebooks").glob("**/*.py"))
-    for s in paths:
-        notebook_str = open(str(s)).read()
+    for f in paths:
+        notebook_str = open(str(f)).read()
         assert notebook_str.startswith("# Databricks notebook source")
